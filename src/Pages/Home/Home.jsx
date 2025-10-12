948e71146062098ddf568e56555a3b7fe74fc901
Riadul Islam <riadul551@gmail.com>
2025-10-12 19:21:32 +0600

    make the hero details

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -8,6 +8,7 @@ import { Link } from "react-router";
 const Home = () => {
   return (
     <div>
+      {/* hero section  */}
       <div>
         {/* hero title  */}
         <div className="py-9">
@@ -46,6 +47,27 @@ const Home = () => {
         {/* hero image  */}
         <div className="flex justify-center">
           <img src={HeroImg} alt="Hero Image" />
+        </div>
+      </div>
+      {/* hero details  */}
+      <div className="bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white py-16">
+        <h1 className="text-center font-extrabold text-4xl">Trusted by Millions, Built for You</h1>
+        <div className="grid grid-cols-3 mt-12 max-w-[1200px] mx-auto">
+          <div className="flex flex-col justify-center items-center">
+            <p className="text-sm font-extralight">Total Downloads</p>
+            <h2 className="font-extrabold text-3xl my-2">29.6M</h2>
+            <p className="text-sm font-extralight">21% more than last month</p>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <p className="text-sm font-extralight">Total Reviews</p>
+            <h2 className="font-extrabold text-3xl my-2">906K</h2>
+            <p className="text-sm font-extralight">46% more than last month</p>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <p className="text-sm font-extralight">Active Apps</p>
+            <h2 className="font-extrabold text-3xl my-2">132+</h2>
+            <p className="text-sm font-extralight">31 more will Launch</p>
+          </div>
         </div>
       </div>
     </div>
