860b32aba142e6713537ebbefde74aa5b96d2f29
Riadul Islam <riadul551@gmail.com>
2025-10-12 19:09:54 +0600

    add links with the hero button

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./Home.css";
 import GooglePlay from "../../assets/Google-play.png";
-import GoogleApp from "../../assets/Google-app.png"
-import HeroImg from "../../assets/hero.png"
+import GoogleApp from "../../assets/Google-app.png";
+import HeroImg from "../../assets/hero.png";
+import { Link } from "react-router";
 
 const Home = () => {
   return (
@@ -27,12 +28,19 @@ const Home = () => {
         {/* hero button  */}
         <div className="text-center mb-9">
           <button className="btn bg-white text-black border-gray-300 m-3 shadow-none">
-            <img src={GooglePlay} width="20px" alt="Google play buttton" />
-            Google Play
+            <img src={GooglePlay} width="20px" alt="Google play buttton" />{" "}
+            <Link to="https://play.google.com/store/games?hl=en">
+              Google Play
+            </Link>
           </button>
           <button className="btn bg-white text-black border-gray-300 m-3 shadow-none">
-            <img className="bg-blue-500" src={GoogleApp} width="20px" alt="Google play buttton" />
-            App Store
+            <img
+              className="bg-blue-500"
+              src={GoogleApp}
+              width="20px"
+              alt="Google play buttton"
+            />
+            <Link to="https://www.apple.com/app-store/">App Store</Link>
           </button>
         </div>
         {/* hero image  */}
