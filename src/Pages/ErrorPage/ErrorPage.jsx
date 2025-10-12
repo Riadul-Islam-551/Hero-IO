ca14af47d4117716bd6d7cb01c018aa5683f4ca9
Riadul Islam <riadul551@gmail.com>
2025-10-12 12:46:20 +0600

    complet the footer part both Root and error page

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,69 +1,40 @@
 import React from "react";
-import { Link, NavLink } from "react-router";
+import { Link } from "react-router";
 import "./ErrorPage.css";
-import { FaGithub } from "react-icons/fa";
-import Logo from "../../assets/logo.png";
+import ErrorImg from "../../assets/error-404.png";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
 
 const ErrorPage = () => {
-  const links = (
-    <>
-      <NavLink to="/" className=" mx-2 p-1 text-sm font-bold">
-        Home
-      </NavLink>
-      <NavLink to="/app" className="mx-2 p-1  text-sm font-bold">
-        App
-      </NavLink>
-      <NavLink to="/installation" className="mx-2 p-1  text-sm font-bold">
-        Installation
-      </NavLink>
-    </>
-  );
   return (
-    <div className="shadow-sm">
-      <div className="navbar  max-w-[1200px] mx-auto">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                {" "}
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />{" "}
-              </svg>
-            </div>
-            <ul
-              tabIndex="-1"
-              className="menu menu-sm dropdown-content bg-gray-100 rounded-box z-10 mt-3 py-2 px-7  shadow"
-            >
-              <div className="flex flex-col">{links}</div>
-            </ul>
-          </div>
-          <img src={Logo} width="20px" alt="" />
-          <Link className="bg-gradient-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent text-lg font-bold">
-            HERO.IO
+    <div>
+      <Navbar></Navbar>
+
+      {/* .............main part .......... */}
+
+      <div className="flex flex-col justify-center items-center">
+        <div className="mt-16">
+          <img src={ErrorImg} alt="Error Image" />
+        </div>
+        <div className="text-center mt-9">
+          <h1 className="text-xl md:text-3xl font-bold">
+            Oops, page not found!
+          </h1>
+          <p className="text-sm text-gray-800">
+            The page you are looking for is not available.
+          </p>
+        </div>
+        <div className="my-9">
+          <Link
+            to="/"
+            className="btn border-0 bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white px-9 py-2 rounded hover:opacity-90"
+          >
+            Go Back!
           </Link>
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">{links}</ul>
-        </div>
-        <div className="navbar-end">
-          <a
-            href="https://github.com/Riadul-Islam-551"
-            className="btn border-0 bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white px-4 py-2 rounded-lg hover:opacity-90"
-          >
-            <FaGithub /> Contribute
-          </a>
-        </div>
       </div>
+      {/* ......footer part ........ */}
+      <Footer></Footer>
     </div>
   );
 };
